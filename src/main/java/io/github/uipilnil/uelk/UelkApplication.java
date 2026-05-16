295e7eb858733f78cae8d93ffea821867ee0cc57
uipilnil <uipilnil@163.com>
2026-05-16 23:56:43 +0800

    初始化仓库

---
+++ src/main/java/io/github/uipilnil/uelk/UelkApplication.java
@@ -0,0 +1,13 @@
+package io.github.uipilnil.uelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UelkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UelkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/uipilnil/uelk/UelkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.uipilnil.uelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UelkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
